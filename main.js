728859432798f2486f3a462f7e8ef8bbebd08558
Federico Fogliatto <fede.fogliatto@gmail.com>
2023-10-07 10:59:26 -0300

    correcciones finales

--- main.js
+++ main.js
@@ -18,8 +18,11 @@ class Producto {
             <div class="col-md-8">
                 <div class="card-body">
                     <h5 class="card-title">${this.nombre}</h5>
-                    <p class="card-text">Cantidad 
-                    ${this.cantidad}</p>
+                    <p class="card-text">Cantidad: <button id="in-${this.id}"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-plus" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                    <path d="M12 5l0 14" />
+                    <path d="M5 12l14 0" />
+                  </svg></button> ${this.cantidad}</p>
                     <p class="card-text">Precio $${this.precio}</p> 
                 </div>
                 <button id="ep-${this.id}"class="btn_tacho"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-trash-filled" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
@@ -61,7 +64,6 @@ class ProductoController {
             let nuevoProducto = new Producto(producto.id, producto.nombre, producto.precio, producto.descripcion, producto.imagen, producto.cantidad)
             this.agregar(nuevoProducto)
         })
-        console.log(listaProductosJs)
         this.mostrarEnDom()
 
     }
@@ -235,5 +237,4 @@ carrito.recuperarStorage()
 carrito.mostrarEnDom()
 
 CP.prepararContenedorProductos()
-CP.cargarProducto()
 CP.mostrarEnDom()
